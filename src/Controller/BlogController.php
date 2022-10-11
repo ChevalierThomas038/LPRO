4fd1510f82fc71dc1c93da767aca0af012965d6f
CHEVALIER THOMAS <tcheva10@sfasp2mi.univ-poitiers.fr>
2022-10-11 16:10:02 +0200

    TP2Fini

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -18,20 +18,31 @@ class BlogController extends AbstractController
     #[Route('/list/{page}', name: 'list',requirements: ['page' => '\d+'], defaults: ['page' => '1'])]
     public function listAction(int $page): Response
     {
-        return $this->render('blog/list.html.twig');
+        $Articles = [];
+
+        for($i=1 ; $i < 5; $i++) {
+            $Articles[] = ['id' => $i, 'article' => 'Article' . strval($i), 'contenue' => 'Ceci est un texte assez
+            long avec lequel nous allons tester notre
+            filtre'];
+        }
+
+        return $this->render('blog/list.html.twig', ['article' => $Articles]);
     }
 
     #[Route('/article/{id}', name: 'article',requirements: ['id' => '\d+'])]
     public function viewAction(int $id): Response
     {
-        return $this->render('blog/view.html.twig');
+        $tabId = ['id' => $id, 'contenue' => 'Ceci est un texte assez
+        long avec lequel nous allons tester notre
+        filtre'];
+        return $this->render('blog/view.html.twig', ['id' => $tabId]);
     }
 
     #[Route('/article/add', name: 'add')]
     public function addAction(): Response
     {
         if(true) {
-            $this->addFlash('info', "Le message flash");
+            $this->addFlash('info', "Article ajouter");
             return $this->redirectToRoute('app_blog_list', ['page'=>'1']);
         }
         return $this->render('blog/blog.html.twig');
@@ -40,14 +51,14 @@ class BlogController extends AbstractController
     #[Route('/article/edit/{id}', name: 'edit',requirements: ['id' => '\d+'], defaults: ['id' => '1'])]
     public function editAction(int $id): Response
     {
-        $this->addFlash('info', "Le message edit");
-        return $this->redirectToRoute('app_blog_article', ['id'=>$id]);
+        $this->addFlash('info', "Le message est edit");
+        return $this->render('blog/edit.html.twig');
     }
 
     #[Route('/article/delete/{id}', name: 'delete',requirements: ['id' => '\d+'], defaults: ['id' => '1'])]
     public function deleteAction(int $id): Response
     {
-        $this->addFlash('info', "Le message suppression");
+        $this->addFlash('info', "Le message est supprimer");
         return $this->redirectToRoute('app_blog_list');
     }
 
